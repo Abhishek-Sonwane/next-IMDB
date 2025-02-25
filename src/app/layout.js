d4c2fddc0ce1ac193ac41ac728705c51598c3238
Abhishek Sonwane <abhisheksonwane629@gmail.com>
2025-02-25 16:04:17 +0530

    init

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,8 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Header from "@/Components/Header";
+import ThemeCom from "@/Components/ThemeCom.js";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -18,12 +21,15 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en" suppressHydrationWarning>
+        <body>
+          <ThemeCom>
+            <Header />
+            {children}
+          </ThemeCom>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
